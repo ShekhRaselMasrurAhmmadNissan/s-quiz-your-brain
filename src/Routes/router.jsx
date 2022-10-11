e5bea2a6c7a38aa376c43761991a3676f6508123
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-11 12:06:46 +0600

    Used the Loader function to call categories Data.

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import Error from '../Components/Error/Error';
 import Home from '../Components/Home/Home';
 import MainLayout from '../Layouts/MainLayout/MainLayout';
+import { getAllQuizCategories } from '../Utilities/Loaders/Loaders';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <MainLayout />,
 		errorElement: <Error />,
+		loader: getAllQuizCategories,
 		children: [
 			{ index: true, element: <Home /> },
 			{ path: 'home', element: <Home /> },
