bf93c72a5158db8529375e41935df70ed2b275d7
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-12 14:23:14 +0600

    Alter some design.

--- src/Components/QuizCategories/QuizCategories.jsx
+++ src/Components/QuizCategories/QuizCategories.jsx
@@ -7,12 +7,17 @@ const QuizCategories = () => {
 	console.log(quizCategories);
 	return (
 		<>
-			{quizCategories.map((singleCategory) => (
-				<SingleQuizCategory
-					key={singleCategory.id}
-					singleCategory={singleCategory}
-				></SingleQuizCategory>
-			))}
+			<h1 className="text-center text-4xl font-bold mt-4 md:mt-8">
+				All Quiz Categories
+			</h1>
+			<div className="mt-4 md:mt-16 md:w-4/5 lg:w-[90%] mx-auto grid grid-cols-1 lg:grid-cols-2 justify-evenly gap-y-8">
+				{quizCategories.map((singleCategory) => (
+					<SingleQuizCategory
+						key={singleCategory.id}
+						singleCategory={singleCategory}
+					></SingleQuizCategory>
+				))}
+			</div>
 		</>
 	);
 };
